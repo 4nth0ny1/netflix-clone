52f4f7bfbb327eaf6dc49e590614ea8474cf6f23
Anthony Catullo <anthonycatullo@oatslam.cable.rcn.com>
2023-05-08 21:24:32 -0500

    added navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
+import Navbar from "../components/Navbar";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,20 +21,9 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-
   return (
     <>
-      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
-      <p className="text-white">
-        Logged in as: {user?.name}, {user?.email}
-      </p>
-      <button
-        className="h-10 p-2 rounded-xl bg-red-500"
-        onClick={() => signOut()}
-      >
-        Logout
-      </button>
+      <Navbar />
     </>
   );
 }
