876a61706987fec789eb70d9f30917aacfc0fa23
Anthony Catullo <anthonycatullo@oatslam.cable.rcn.com>
2023-05-10 17:14:10 -0500

    added infomodal, account name, zustand state mgmt

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,10 +1,16 @@
-import useBillboard from "@/hooks/useBillboard";
-import React from "react";
+import React, { useCallback } from "react";
 import { AiOutlineInfoCircle } from "react-icons/ai";
-import PlayButton from "./PlayButton";
+import PlayButton from "@/components/PlayButton";
+import useBillboard from "@/hooks/useBillboard";
+import useInfoModalStore from "@/hooks/useInfoModalStore";
 
 const Billboard = () => {
+  const { openModal } = useInfoModalStore();
   const { data } = useBillboard();
+
+  const handleOpenModal = useCallback(() => {
+    openModal(data?.id);
+  }, [openModal, data?.id]);
   return (
     <div className="relative h-[56.25vw]">
       <video
@@ -25,6 +31,7 @@ const Billboard = () => {
         <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
           <PlayButton movieId={data?.id} />
           <button
+            onClick={handleOpenModal}
             className="
             bg-white
             text-white
--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import FavoriteButton from "./FavoriteButton";
 import { BsFillPlayFill } from "react-icons/bs";
+import { BiChevronDown } from "react-icons/bi";
 import { useRouter } from "next/router";
+import useInfoModalStore from "@/hooks/useInfoModalStore";
+import { MovieInterface } from "@/types";
 
 interface MovieCardProps {
-  data: Record<string, any>;
+  data: MovieInterface;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const router = useRouter();
+  const { openModal } = useInfoModalStore();
 
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
@@ -88,6 +92,12 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
             </div>
             <div className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
               <FavoriteButton movieId={data?.id} />
+            </div>
+            <div
+              onClick={() => openModal(data?.id)}
+              className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300"
+            >
+              <BiChevronDown className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
             </div>
           </div>
           <p className="text-green-400 font-semibold mt-4">
